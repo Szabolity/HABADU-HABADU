dbb16a6f35a22a9fb409f1d93ce9581a5e2e65a7
Szabolity <190555705+Szabolity@users.noreply.github.com>
2025-01-16 09:42:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,24 +25,24 @@ navigator.mediaDevices.getUserMedia({ audio: true })
                 message.textContent = "HABADU HABADU ATE KU! 🎉";
                 
                 confetti({
-                    particleCount: 100,
+                    particleCount: 50,
                     spread: 70,
                     origin: { y: 0.6 }
                 });
 
                 birthdaySong.play();
 
-                const duration = 2 * 1000;
+                const duration = 1 * 1000;
                 const end = Date.now() + duration;
                 (function frame() {
                     confetti({
-                        particleCount: 5,
+                        particleCount: 3,
                         angle: 60,
                         spread: 55,
                         origin: { x: 0 }
                     });
                     confetti({
-                        particleCount: 5,
+                        particleCount: 3,
                         angle: 120,
                         spread: 55,
                         origin: { x: 1 }
