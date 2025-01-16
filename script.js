dd482e4aca72bdbd77753a46ae5753c9d51c2d2b
Szabolity <190555705+Szabolity@users.noreply.github.com>
2025-01-16 09:44:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -35,19 +35,6 @@ navigator.mediaDevices.getUserMedia({ audio: true })
                 const duration = 1 * 1000;
                 const end = Date.now() + duration;
                 (function frame() {
-                    confetti({
-                        particleCount: 3,
-                        angle: 60,
-                        spread: 55,
-                        origin: { x: 0 }
-                    });
-                    confetti({
-                        particleCount: 3,
-                        angle: 120,
-                        spread: 55,
-                        origin: { x: 1 }
-                    });
-
                     if (Date.now() < end) {
                         requestAnimationFrame(frame);
                     }
